dd73f592fff992ed4997e2500de8f2236dee3f65
straho99 <strahil.ruychev@gmail.com>
2015-11-08 11:00:39 +0200

    Updated Db Models

---
+++ ConferenceManager/ConferenceManager.Models/Message.cs
@@ -0,0 +1,23 @@
+﻿namespace ConferenceManager.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Message
+    {
+        [Key]
+        public int Key { get; set; }
+
+        [Required]
+        public int SenderId { get; set; }
+
+        public User Sender { get; set; }
+
+        [Required]
+        public int RecipientId { get; set; }
+
+        public User Recipient { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
