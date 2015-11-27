ed9745f049eb7efe10b56b9627ddb517dbc307ee
Strahil Ruychev <strahil.ruychev@gmail.com>
2015-11-27 17:31:58 +0200

    Create AdminServices.php

---
+++ RedDevil/Services/AdminServices.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace RedDevil\Services;
+
+use RedDevil\View;
+use RedDevil\InputModels\Admin\AddRoleInputModel;
+use RedDevil\InputModels\Admin\UsersRolesViewModel;
+
+class AdminServices extends BaseService {
+
+    public function manageRoles()
+    {
+        
+    }
+    
+    public function addRole(AddRoleInputModel $model)
+    {
+        
+    }
+}
